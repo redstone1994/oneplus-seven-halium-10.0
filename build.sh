0d0e7fa4ada1ce2c328646c031819baf6500c8aa
RyanAM <ram920054@gmail.com>
2024-11-08 17:53:52 -0700

    fix build scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -xe
+shopt -s extglob
 
 BUILD_DIR=
 OUT=
+ONLY_CLONE=
+ONLY_KERNEL=
+MENUCONFIG=
 
 while [ $# -gt 0 ]
 do
     case "$1" in
     (-b) BUILD_DIR="$(realpath "$2")"; shift;;
     (-o) OUT="$2"; shift;;
+    (-c) ONLY_CLONE="true";;
+    (-k) ONLY_KERNEL="true";;
+    (-m) MENUCONFIG="true";;
     (-*) echo "$0: Error: unknown option $1" 1>&2; exit 1;;
     (*) OUT="$2"; break;;
     esac
@@ -39,75 +46,50 @@ fi
 mkdir -p "${TMP}/system" "${TMP}/partitions"
 
 source "${HERE}/deviceinfo"
-
-case $deviceinfo_arch in
-    "armhf") RAMDISK_ARCH="armhf";;
-    "aarch64") RAMDISK_ARCH="arm64";;
-    "x86") RAMDISK_ARCH="i386";;
-esac
-
-cd "$TMPDOWN"
-    [ -d aarch64-linux-android-4.9 ] || git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b pie-gsi --depth 1
-    GCC_PATH="$TMPDOWN/aarch64-linux-android-4.9"
-    if $deviceinfo_kernel_clang_compile; then
-        [ -d linux-x86 ] || git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android10-gsi --depth 1
-        CLANG_PATH="$TMPDOWN/linux-x86/clang-r353983c"
-    fi
-    if [ "$deviceinfo_arch" == "aarch64" ]; then
-        [ -d arm-linux-androideabi-4.9 ] || git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b pie-gsi --depth 1
-        GCC_ARM32_PATH="$TMPDOWN/arm-linux-androideabi-4.9"
-    fi
-    KERNEL_DIR="$(basename "${deviceinfo_kernel_source}")"
-    KERNEL_DIR="${KERNEL_DIR%.*}"
-    [ -d "$KERNEL_DIR" ] || git clone "$deviceinfo_kernel_source" -b $deviceinfo_kernel_source_branch --depth 1 --recursive
-
-    if [ ! -f halium-boot-ramdisk.img ]; then
-        if [[ "$deviceinfo_kernel_cmdline" == *"systempart=/dev/mapper"* ]]; then
-            RAMDISK_URL="https://github.com/halium/initramfs-tools-halium/releases/download/dynparts/initrd.img-touch-${RAMDISK_ARCH}"
-        else
-            RAMDISK_URL="https://github.com/halium/initramfs-tools-halium/releases/download/continuous/initrd.img-touch-${RAMDISK_ARCH}"
-        fi
-        curl --location --output halium-boot-ramdisk.img "$RAMDISK_URL"
-    fi
-
-    if ([ -n "$deviceinfo_kernel_apply_overlay" ] && $deviceinfo_kernel_apply_overlay) || [ -n "$deviceinfo_dtbo" ]; then
-        [ -d libufdt ] || git clone https://android.googlesource.com/platform/system/libufdt -b pie-gsi --depth 1
-        [ -d dtc ] || git clone https://android.googlesource.com/platform/external/dtc -b pie-gsi --depth 1
-    fi
-
-    [ -d "avb" ] || git clone https://android.googlesource.com/platform/external/avb -b android13-gsi --depth 1
-
-    if [ -n "$deviceinfo_kernel_use_dtc_ext" ] && $deviceinfo_kernel_use_dtc_ext; then
-        [ -f "dtc_ext" ] || curl --location https://android.googlesource.com/platform/prebuilts/misc/+/refs/heads/android10-gsi/linux-x86/dtc/dtc?format=TEXT | base64 --decode > dtc_ext
-        chmod +x dtc_ext
-    fi
-
-    if [ ! -f "vbmeta.img" ] && [ -n "$deviceinfo_bootimg_append_vbmeta" ] && $deviceinfo_bootimg_append_vbmeta; then
-        wget https://dl.google.com/developers/android/qt/images/gsi/vbmeta.img
-    fi
-
-    ls .
-cd "$HERE"
+source "$SCRIPT/common_functions.sh"
+source "$SCRIPT/setup_repositories.sh" "${TMPDOWN}"
+if [ "$ONLY_CLONE" = "true" ]; then
+    exit 0
+fi
 
 if [ -n "$deviceinfo_kernel_apply_overlay" ] && $deviceinfo_kernel_apply_overlay; then
     "$SCRIPT/build-ufdt-apply-overlay.sh" "${TMPDOWN}"
 fi
 
-if [ -n "$deviceinfo_kernel_use_dtc_ext" ] && $deviceinfo_kernel_use_dtc_ext; then
-    export DTC_EXT="$TMPDOWN/dtc_ext"
-fi
-
 if $deviceinfo_kernel_clang_compile; then
-    if [ -n "$deviceinfo_kernel_use_lld" ] && $deviceinfo_kernel_use_lld; then
-        export LD=ld.ldd
+    if [ -n "$deviceinfo_kernel_llvm_compile" ] && $deviceinfo_kernel_llvm_compile; then
+        # Restrict available binaries in PATH to make builds less susceptible to host differences
+        ALLOWED_HOST_TOOLS="bash git perl sh sync tar"
+
+        HOST_TOOLS=${TMPDOWN}/host_tools
+        rm -rf ${HOST_TOOLS}
+        mkdir -p ${HOST_TOOLS}
+        for tool in ${ALLOWED_HOST_TOOLS}
+        do
+            ln -sf $(which $tool) ${HOST_TOOLS}
+        done
+
+        BUILD_TOOLS_BIN="${TMPDOWN}/build-tools/linux-x86/bin"
+        BUILD_TOOLS_PATH="${TMPDOWN}/build-tools/path/linux-x86"
+
+        EXTRA_TOYBOX_TOOLS="dd expr nproc tr"
+        for tool in ${EXTRA_TOYBOX_TOOLS}
+        do
+            ln -sf ../../linux-x86/bin/toybox "${BUILD_TOOLS_PATH}/${tool}"
+        done
+
+        KERNEL_BUILD_TOOLS_BIN="${TMPDOWN}/kernel-build-tools/linux-x86/bin"
+
+        PATH="$CLANG_PATH/bin:${BUILD_TOOLS_BIN}:${BUILD_TOOLS_PATH}:${KERNEL_BUILD_TOOLS_BIN}:${HOST_TOOLS}" \
+            "$SCRIPT/build-kernel.sh" "${TMPDOWN}" "${TMP}/system" "${MENUCONFIG}"
+    else
+        CC=clang \
+            PATH="$CLANG_PATH/bin:$GCC_PATH/bin:$GCC_ARM32_PATH/bin:${PATH}" \
+            "$SCRIPT/build-kernel.sh" "${TMPDOWN}" "${TMP}/system" "${MENUCONFIG}"
     fi
-    CC=clang \
-    CLANG_TRIPLE=${deviceinfo_arch}-linux-gnu- \
-    PATH="$CLANG_PATH/bin:$GCC_PATH/bin:$GCC_ARM32_PATH/bin:${PATH}" \
-    "$SCRIPT/build-kernel.sh" "${TMPDOWN}" "${TMP}/system"
 else
     PATH="$GCC_PATH/bin:$GCC_ARM32_PATH/bin:${PATH}" \
-    "$SCRIPT/build-kernel.sh" "${TMPDOWN}" "${TMP}/system"
+        "$SCRIPT/build-kernel.sh" "${TMPDOWN}" "${TMP}/system" "${MENUCONFIG}"
 fi
 
 # If deviceinfo_skip_dtbo_partition is set to true, do not copy an image for dedicated dtbo partition.
@@ -120,7 +102,11 @@ if [ -z "$deviceinfo_skip_dtbo_partition" ] || ! $deviceinfo_skip_dtbo_partition
     fi
 fi
 
-"$SCRIPT/make-bootimage.sh" "${TMPDOWN}" "${TMPDOWN}/KERNEL_OBJ" "${TMPDOWN}/halium-boot-ramdisk.img" "${TMP}/partitions/boot.img"
+"$SCRIPT/make-bootimage.sh" "${TMPDOWN}" "${TMPDOWN}/KERNEL_OBJ" "${TMPDOWN}/halium-boot-ramdisk.img" \
+    "${TMP}/partitions/boot.img" "${TMP}/system"
+if [ "$ONLY_KERNEL" = "true" ]; then
+    exit 0
+fi
 
 cp -av overlay/* "${TMP}/"
 
@@ -153,14 +139,17 @@ while IFS= read -r path ; do
 done <<< "$BUILDPROP_PATHS"
 
 if [ -z "$deviceinfo_use_overlaystore" ]; then
-    "$SCRIPT/build-tarball-mainline.sh" "${deviceinfo_codename}" "${OUT}" "${TMP}"
     # create device tarball for https://wiki.debian.org/UsrMerge rootfs
     "$SCRIPT/build-tarball-mainline.sh" "${deviceinfo_codename}" "${OUT}" "${TMP}" "usrmerge"
 else
     "$SCRIPT/build-tarball-mainline.sh" "${deviceinfo_codename}" "${OUT}" "${TMP}" "overlaystore"
-    # create a symlink for _usrmerge variant so that common pipeline just works.
-    ln -sf "device_${deviceinfo_codename}.tar.xz" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.xz"
 fi
+# compatibility symlink for  _usrmerge variant so that old pipelines just work
+ln -f "${OUT}/device_${deviceinfo_codename}.tar.xz" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.xz"
+ln -f "${OUT}/device_${deviceinfo_codename}.tar.build" "${OUT}/device_${deviceinfo_codename}_usrmerge.tar.build"
+
+# Upload Module.symvers to artifacts for GKI debugging
+[ -f "${TMPDOWN}/KERNEL_OBJ/Module.symvers" ] && cp "${TMPDOWN}/KERNEL_OBJ/Module.symvers" "${OUT}"
 
 if [ -z "$BUILD_DIR" ]; then
     rm -r "${TMP}"
